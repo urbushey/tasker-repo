ef2626fa1c06a9b21afd440cb076ccab07f91399
urbushey <urbushey@gmail.com>
2013-01-14 02:27:01 -0500

    Added Twitter bootstrap and basic template

--- tasker/tasker/urls.py
+++ tasker/tasker/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
 
-
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
 
@@ -11,4 +10,5 @@ urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/$', 'apps.taskerapp.views.index'),
 )
